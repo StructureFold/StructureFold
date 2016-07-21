26e63023f45578e5ec791c0e24e63e93727759ef
Yin Tang <yintang@YINS-MAC-PRO.bio.psu.edu>
2016-07-21 12:25:04 -0400

    add write_csv function

--- header/write_file.py
+++ header/write_file.py
@@ -26,4 +26,22 @@ def write_t_file(out_file, a):
             h.write(str(a)+"\n")
 
 
-#write_file("result.txt", [["1"], [1,2]])
+def write_c_file(out_file, a):
+    with open(out_file, 'w') as h:
+        if type(a) is list:
+            for i in range(len(a)):
+                if type(a[i]) is list:
+                    if len(a[i])>1:
+                        for j in range(len(a[i])-1):
+                            h.write(str(a[i][j])+",")
+                        j = j+1
+                        h.write(str(a[i][j])+"\n")
+                    else:
+                        if len(a[i]) == 1:
+                            h.write(str(a[i][0])+"\n")
+                        else:
+                            h.write("\n")
+                else:
+                    h.write(str(a[i])+"\n")
+        else:
+            h.write(str(a)+"\n")
